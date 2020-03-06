67abcc5ac5c5fe356a92d5617fe459f396a78cb3
C1313347 Matheus Sena Vasconcelos <c1313347@interno.bb.com.br>
2020-03-06 14:08:56 -0300

    transformed fit_predict method as concret method

--- dtypes/base.py
+++ dtypes/base.py
@@ -10,8 +10,10 @@ class BaseDType(abc.ABC):
     @abc.abstractmethod
     def predict(self, x: Sequence[str]) -> Sequence[Sequence[int]]: ...
 
-    @abc.abstractmethod
-    def fit_predict(self, x: Sequence[str]) -> Sequence[Sequence[int]]: ...
+    def fit_predict(self, x: Sequence[str]) -> Sequence[Sequence[int]]:
+        self.fit(x)
+        output = self.predict(x)
+        return output
 
     def __repr__(self):
         return f'<class {type(self).__name__}>'
